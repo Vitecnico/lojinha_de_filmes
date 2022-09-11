0c7b7af0c50d0cfe7190cad01cc04364fee610ba
Vitor AP <vitorap.tecnico@gmail.com>
2022-09-11 15:42:15 -0300

    Utilização do pmdb.js no App.js, adição de scripts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Tmdb from "./API/Tmdb";
 
 export default () => {
+  
+  const [movieList, setMovieList] = useState([]);
+  
+  useEffect(() =>{
+    const loadTmdbData = async() =>{
+      //Pegando a lista total do Tmdb.js
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+    }
+
+    loadTmdbData();
+
+  },[]);
+  
+  
   return(
-    <div>
-      Teste
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            {item.title}
+          </div>
+        ))}
+
+      </section>
+      
     </div>
-  )
+  );
 
  
 }
