322aead2ae05e4c3b1676c2c1a8fb2f5d725a7f9
Vitor AP <vitorap.tecnico@gmail.com>
2022-09-11 22:36:03 -0300

    começando a trabalhar na movimentação das listas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./API/Tmdb";
+import "./App.css"
 import MovieListRow from "./componentesVisuais/MovieListRow";
 
 export default () => {
---
+++ src/componentesVisuais/MovieListRow/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './MovieListRow.css';
+import {MdNavigateBefore} from 'react-icons/md'
+import {MdNavigateNext} from 'react-icons/md'
+
+export default ({title,itens}) => {
+    return(
+
+        <div className="movieListRow">
+            <h2>{title}</h2>
+            <div className="movieListRow--left">
+                <MdNavigateBefore style ={{fontSize: 1000}} />
+            </div>
+            <div className="movieListRow--right">
+                <MdNavigateNext style ={{fontsize: 1000}} />
+            </div>
+            
+            <div className="movieListRow--listarea">
+                <div className="movieListRow--list">
+                    {itens.results.lenght =! 0 && itens.results.map((item, key) =>(
+                        <div key = {key} className="movieListRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt = {item.original_title}/>
+                        </div>
+                        
+                    ))}
+                </div>
+
+            </div>
+        </div>
+    );
+
+}
